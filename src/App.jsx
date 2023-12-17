0e123514c6a7d6bb3b75d28964db74f8c6ad7ef7
CoderDotJs <md.akash6724@gmail.com>
2023-12-17 20:47:33 +0600

    build: new ui and changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,58 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
-import Tables from './pages/Tables'
-import Billing from './pages/Billing'
-import Rtl from './pages/Rtl'
-import Profile from './pages/Profile'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import Main from './components/layout/Main'
-import 'antd/dist/reset.css'
-import './assets/styles/main.css'
-import './assets/styles/responsive.css'
+import 'antd/dist/reset.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './assets/styles/main.css';
+import './assets/styles/responsive.css';
+import Main from './components/layout/Main';
+import Billing from './pages/Billing';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Rtl from './pages/Rtl';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Tables from './pages/Tables';
 
 const router = createBrowserRouter([
-    {
-        path: '/sign-up',
-        element: <SignUp />,
-    },
-    {
-        path: '/sign-in',
-        element: <SignIn />,
-    },
-    {
-        path: '/',
-        element: <Main />,
-        // loader: rootLoader,
-        children: [
-            {
-                path: 'dashboard',
-                element: <Home />,
-            },
-            {
-                path: 'tables',
-                element: <Tables />,
-            },
-            {
-                path: 'billing',
-                element: <Billing />,
-            },
-            {
-                path: 'rtl',
-                element: <Rtl />,
-            },
-            {
-                path: 'profile',
-                element: <Profile />,
-            },
-        ],
-    },
-])
+  {
+    path: '/sign-up',
+    element: <SignUp />,
+  },
+  {
+    path: '/sign-in',
+    element: <SignIn />,
+  },
+  {
+    path: '/',
+    element: <Main />,
+    // loader: rootLoader,
+    children: [
+      { index: true, element: <Home /> },
+      { index: true, path: 'dashboard', element: <Home /> },
+      {
+        path: 'tables',
+        element: <Tables />,
+      },
+      {
+        path: 'billing',
+        element: <Billing />,
+      },
+      {
+        path: 'rtl',
+        element: <Rtl />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+]);
 
 function App() {
-    return (
-        <div className="App">
-            <RouterProvider router={router} />
-        </div>
-    )
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,71 +1,58 @@
-import { HeartFilled } from '@ant-design/icons'
-import { Col, Layout, Row } from 'antd'
+import { HeartFilled } from '@ant-design/icons';
+import { Col, Layout, Row } from 'antd';
 
 function Footer() {
-    const { Footer: AntFooter } = Layout
+  const { Footer: AntFooter } = Layout;
 
-    return (
-        <AntFooter style={{ background: '#fafafa' }}>
-            <Row className="just">
-                <Col xs={24} md={12} lg={12}>
-                    <div className="copyright">
-                        © 2021, made with
-                        {<HeartFilled />} by
-                        <a
-                            href="#pablo"
-                            className="font-weight-bold"
-                            target="_blank"
-                        >
-                            Creative Tim
-                        </a>
-                        for a better web.
-                    </div>
-                </Col>
-                <Col xs={24} md={12} lg={12}>
-                    <div className="footer-menu">
-                        <ul>
-                            <li className="nav-item">
-                                <a
-                                    href="#pablo"
-                                    className="nav-link text-muted"
-                                    target="_blank"
-                                >
-                                    Creative Tim
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a
-                                    href="#pablo"
-                                    className="nav-link text-muted"
-                                    target="_blank"
-                                >
-                                    About Us
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a
-                                    href="#pablo"
-                                    className="nav-link text-muted"
-                                    target="_blank"
-                                >
-                                    Blog
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a
-                                    href="#pablo"
-                                    className="nav-link pe-0 text-muted"
-                                    target="_blank"
-                                >
-                                    License
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </Col>
-            </Row>
-        </AntFooter>
-    )
+  return (
+    <AntFooter style={{ background: '#fafafa' }}>
+      <Row className="just">
+        <Col xs={24} md={12} lg={12}>
+          <div className="copyright">
+            © 2021, made with
+            {<HeartFilled />} from
+            <a href="#akash" className="font-weight-bold" target="_blank">
+              Bangladesh
+            </a>
+            for a better web.
+          </div>
+        </Col>
+        <Col xs={24} md={12} lg={12}>
+          <div className="footer-menu">
+            <ul>
+              <li className="nav-item">
+                <a
+                  href="#akash"
+                  className="nav-link text-muted"
+                  target="_blank"
+                >
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#akash"
+                  className="nav-link text-muted"
+                  target="_blank"
+                >
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#akash"
+                  className="nav-link pe-0 text-muted"
+                  target="_blank"
+                >
+                  License
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </AntFooter>
+  );
 }
 
-export default Footer
+export default Footer;
